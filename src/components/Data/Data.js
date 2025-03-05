d4d56c09dfd03659597b2d1db4fb703c45c5032b
oleveloper <oleveloper@gmail.com>
2025-03-05 17:03:04 +0900

    feat: replace loading text with throbber UI

--- src/components/Data/Data.js
+++ src/components/Data/Data.js
@@ -3,6 +3,23 @@ import { useParams } from "react-router-dom";
 import config from '../../config';
 import axios from "axios";
 import FileTable from "../Table/FileTable";
+import CircularProgress from "@mui/material/CircularProgress";
+
+function GradientCircularProgress() {
+  return (
+    <React.Fragment>
+      <svg width={0} height={0}>
+        <defs>
+          <linearGradient id="my_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop offset="0%" stopColor="#e01cd5" />
+            <stop offset="100%" stopColor="#1CB5E0" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <CircularProgress sx={{ 'svg circle': { stroke: 'url(#my_gradient)' } }} />
+    </React.Fragment>
+  );
+}
 
 function Data({ setVersions }) {
   const API_BASE_URL = config.API_BASE_URL;
@@ -24,7 +41,11 @@ function Data({ setVersions }) {
     }, [category]);
 
   if (loading) {
-    return <p>Loading...</p>;
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <GradientCircularProgress />
+      </div>
+    );
   }
 
   return (
