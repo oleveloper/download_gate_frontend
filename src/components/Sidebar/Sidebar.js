85a2b893d5a6d40406cebbd84c69a0bac331c22b
oleveloper <oleveloper@gmail.com>
2025-03-30 17:18:14 +0900

    feat: add hotfix list and version sorting functionality

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -10,12 +10,26 @@ const Sidebar = ({versions = []}) => {
   const handleClick = () => {
     navigate('/');
   };
+  const sortedVersions = [...versions].sort((a, b) => {
+    const aParts = a.split('.').map(Number);
+    const bParts = b.split('.').map(Number);
+
+    const len = Math.max(aParts.length, bParts.length);
+    for (let i = 0; i < len; i++) {
+      const aNum = aParts[i] || 0;
+      const bNum = bParts[i] || 0;
+      if (aNum !== bNum) return bNum - aNum;
+    }
+    return 0;
+  })
 
   useEffect(() => {
     if (location.pathname.startsWith('/install')) {
       setActiveMenu('install_files');
     } else if (location.pathname.startsWith('/patch')) {
       setActiveMenu('patch_files');
+    } else if (location.pathname.startsWith('/hotfix')) {
+      setActiveMenu('hotfix_files');
     } else if (location.pathname.startsWith('/jdk')) {
       setActiveMenu('jdk_files');
     } else if (location.pathname.startsWith('/license')){
@@ -30,7 +44,7 @@ return (
 <div className="sidebar">
   <div className="logo">
     <div className="logo-icon"></div>
-    <span onClick={handleClick} style={{ cursor: 'pointer' }}>APP</span>
+    <span onClick={handleClick} style={{ cursor: 'pointer' }}>LENA LAB</span>
   </div>
   <nav className="menu">
     <p>DOWNLOAD</p>
@@ -40,7 +54,8 @@ return (
       </ProtectedLink>
       {activeMenu === 'install_files' && (
         <div className="submenu" >
-          {versions.map((v) => (
+          <div className="install-list-scroll">
+          {sortedVersions.map((v) => (
             <ProtectedLink 
               key={v}
               to={`/install/versions/${v}`}
@@ -51,12 +66,18 @@ return (
               {v}
             </ProtectedLink>
           ))}
+          </div>
         </div>
       )}
     </div>
     <div className='menu-label'>
       <ProtectedLink to={`/patch`}>
           Patch Files
+      </ProtectedLink>
+    </div>
+    <div className='menu-label'>
+      <ProtectedLink to={`/hotfix`}>
+          Hotfix Files
       </ProtectedLink>
     </div>
     <div className='menu-label'>
